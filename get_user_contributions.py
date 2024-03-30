6f5774c3a1cffcad65f36bbe9222785b5c5ca85b
tobiadefami <tobiadefami@gmail.com>
2024-03-30 18:02:49 +0100

    remove fork connection

--- get_user_contributions.py
+++ get_user_contributions.py
@@ -1,21 +1,46 @@
-import requests
+# import requests
 import os
+import re
 import asyncio
 import aiohttp
 from pprint import pprint
-
+from collections import defaultdict
 
 GITHUB_API_KEY = os.getenv("GITHUB_API_KEY")
+NEXT_PATTERN = re.compile(r'(?<=<)([\S]*)(?=>; rel="next")', re.IGNORECASE)
+
+
+async def get_contributors(
+    repository_full_name: str, session: aiohttp.client.ClientSession = None
+) -> list[str]:
+
+    print("getting collaborators of", repository_full_name)
+    url = f"https://api.github.com/repos/{repository_full_name}/contributors"
+
+    async with session.get(url) as response:
+        if response.status in (204, 403, 404):
+            return []
+        if response.status != 200:
+            print(response.status)
+            raise Exception(response.content)
+
+        contributors = await response.json()
+
+        return [contrib["login"] for contrib in contributors]
 
 
 async def get_repositories_by_user(
-    user_name: str, results_per_page: int = 30, type: str = "all"
-) -> list:
+    user_name: str,
+    results_per_page: int = 100,
+    type: str = "all",
+    session: aiohttp.client.ClientSession = None,
+) -> list[str]:
     """curl -L \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer <YOUR-TOKEN>" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     """
+    print("getting repos of", user_name)
     url = f"https://api.github.com/users/{user_name}/repos?type={type}?&per_page={results_per_page}"
 
     headers = {
@@ -23,30 +48,48 @@ async def get_repositories_by_user(
         "Authorization": f"Bearer {GITHUB_API_KEY}",
         "X-GitHub-Api-Version": "2022-11-28",
     }
-    async with aiohttp.client.ClientSession() as session:
-        async with session.get(url, headers=headers) as response:
-            if response.status != 200:
-                return []
-            result = []
-            repositories = await response.json()
-            for repo in repositories:
-                user_url = repo["owner"]["url"]
-                async with session.get(user_url, headers=headers) as user_response:
-                    user_data = await user_response.json()
-                    repo_data = {
-                        "repository_name": repo["full_name"],
-                        "repository_id": repo["id"],
-                        "user_data": user_data
-                    }
-                    result.append(repo_data)
-                   
-            return result[:5]
+
+    results = []
+    async with session.get(url, headers=headers) as response:
+        if response.status in (204, 403, 404):
+            return []
+
+        if response.status != 200:
+            print(response.status)
+            raise Exception(response.content)
+        results.extend(await response.json())
+        header = response.headers.get("Link")
+        next_link = NEXT_PATTERN.search(header)
+
+        while next_link:
+            async with session.get(next_link.group(0), headers=headers) as response:
+                results.extend(await response.json())
+                header = response.headers.get("Link")
+                next_link = NEXT_PATTERN.search(header)
+
+        return [repo["full_name"] for repo in results if not repo["fork"]]
+
+
+async def get_collaborators(
+    user_name: str, session: aiohttp.client.ClientSession = None
+) -> dict[str, set[str]]:
+    result: dict[str, set[str]] = defaultdict(set)
+    print("getting collaborators of", user_name)
+    repository_full_names = await get_repositories_by_user(user_name, session=session)
+    for repository_full_name in repository_full_names:
+        contributors = await get_contributors(repository_full_name, session=session)
+        for contributor in contributors:
+            print(contributor, repository_full_name)
+            result[contributor].add(repository_full_name)
+    return result
 
 
 async def main(user_name: str):
-    repositories = await get_repositories_by_user(user_name)
+
+    async with aiohttp.ClientSession() as session:
+        repositories = await get_collaborators(user_name, session)
     pprint(repositories)
 
 
 if __name__ == "__main__":
-    asyncio.run(main("tobiadefami"))
+    asyncio.run(main("madisonmay"))
