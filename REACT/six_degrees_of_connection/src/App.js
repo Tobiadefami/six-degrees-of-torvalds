7706450cb96dedf1a20132645212e6a1dfc4362c
tobiadefami <tobiadefami@gmail.com>
2024-06-14 18:02:20 +0100

    further ui tweaks

--- REACT/six_degrees_of_connection/src/App.js
+++ REACT/six_degrees_of_connection/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import Home from "./home";
-import Login from "./login";
 
 function App() {
   const [setUsername] = useState("");
@@ -11,8 +10,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home onUsernameSubmit={setUsername} />} />
-        <Route path="/" Component={<Login />} />
-        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
