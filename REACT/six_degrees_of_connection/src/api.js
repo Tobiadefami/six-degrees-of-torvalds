e22aadf27f269473f5ccf3249cdb419bd1f393e2
tobiadefami <tobiadefami@gmail.com>
2024-06-20 13:31:43 +0100

    rm old files

--- REACT/six_degrees_of_connection/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-console.log(process.env.REACT_APP_BACKEND_HOST);
-const api = axios.create({
-  baseURL: "http://" + process.env.REACT_APP_BACKEND_HOST,
-  withCredentials: true,
-});
-
-export default api;
